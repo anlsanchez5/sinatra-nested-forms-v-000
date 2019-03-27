8761394128ae90254b00180c0ac8ed37f66661f3
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-27 21:33:29 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  attr_accessor :name, :booty_attributes
+  attr_reader :type
+
+  @@all = []
+
+  def initialize(arg)
+    @name = arg[:name]
+    @type = arg[:type]
+    @booty_attributes = arg[:booty_attributes]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
 end

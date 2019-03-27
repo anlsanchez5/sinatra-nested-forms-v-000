b599850de5793b281764541da816d03fdaf58cca
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-27 21:38:10 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,4 +1,9 @@
 class Pirate
   attr_reader :name, :height
   attr_accessor :weight
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
 end
